ecfbf122274fb2207713d5674e68c47b541d6037
Hailey-Z <hailey.zhang1@monash.edu>
2023-06-27 15:49:09 +1000

    add GA4 google analytics file

--- ui.R
+++ ui.R
@@ -81,6 +81,7 @@ ui <- function(request){shinyUI(
     dashboardBody(
       useShinyjs(), #imp to use shinyjs functions
       tags$head(includeScript("google_analytics.js")),
+      tags$head(includeHTML(("google_analytics-GA4.html"))),
      
       tags$head(
         tags$link(rel = "stylesheet", type = "text/css", href = "./css/custom.css")
